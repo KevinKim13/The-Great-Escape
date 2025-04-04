126441ab6a1f2cb30df3e1c82cadabedd350a158
KevinKim13 <kimk13@miamioh.edu>
2025-04-04 10:35:09 -0400

    Made a main mehod for the game to run in.

---
+++ Main.java
@@ -0,0 +1,15 @@
+/**
+ * The main method which intializes and runs the text-based game.
+ * 
+ * @author Kevin
+ */
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+
+}
